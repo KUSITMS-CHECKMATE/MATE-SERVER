6bc56f6bfe328bdee22816cc3f117d6c8c9d620c
Rossenzii <jangmj80@naver.com>
2026-04-04 15:22:05 +0900

    initial commit

---
+++ src/test/java/server/MATE/MateApplicationTests.java
@@ -0,0 +1,13 @@
+package server.MATE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
