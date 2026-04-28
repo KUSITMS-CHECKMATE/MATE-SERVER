93844bfa3886d76f4265d282d66272f93e80de5a
hyunbin <s9476721@naver.com>
2026-04-29 03:44:06 +0900

    feat: BaseEntity, Azure Blob 이미지 업로드 서비스 구현 (#8)

---
+++ src/main/java/server/MATE/global/image/LocalImageService.java
@@ -0,0 +1,22 @@
+package server.MATE.global.image;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+// TODO: Azure Storage 연동 완료 후 이 클래스와 @Profile 분기 제거
+// AzureBlobImageService에 @Profile("prod") 제거 후 단일 구현체로 통합
+@Slf4j
+@Service
+@Profile("local")
+public class LocalImageService implements ImageService {
+
+    @Override
+    public List<String> uploadFiles(List<MultipartFile> files) {
+        log.info("LocalImageService: 로컬 환경에서는 이미지 업로드를 건너뜁니다. ({}개)", files.size());
+        return List.of();
+    }
+}
