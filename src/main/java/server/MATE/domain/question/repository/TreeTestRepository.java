298fac7b7265893907f7b4b418901ed0496d95e8
Rossenzii <jangmj80@naver.com>
2026-04-30 17:58:15 +0900

    chore: cardsorting 제외 파일 제거(#6)

--- src/main/java/server/MATE/domain/question/repository/TreeTestRepository.java
+++
@@ -1,7 +0,0 @@
-package server.MATE.domain.question.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import server.MATE.domain.question.entity.TreeTest;
-
-public interface TreeTestRepository extends JpaRepository<TreeTest, Long> {
-}
