5748f719fb1a3fd3e00dc068770aca785ca89b93
happine2s <happine2s@sookmyung.ac.kr>
2026-05-05 05:23:34 +0900

    feat: Users jpa 엔티티 구현 (#26)

---
+++ src/main/java/server/MATE/domain/users/repository/UsersRepository.java
@@ -0,0 +1,9 @@
+package server.MATE.domain.users.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import server.MATE.domain.users.entity.Users;
+
+import java.util.Optional;
+
+public interface UsersRepository extends JpaRepository<Users, Long> {
+}
