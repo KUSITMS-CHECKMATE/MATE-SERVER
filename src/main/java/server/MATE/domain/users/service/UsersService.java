707afa49807fc25169a07103573542d496ebac82
happine2s <happine2s@sookmyung.ac.kr>
2026-05-05 06:34:11 +0900

    feat: 내 정보 조회 api 추가 (#26)

---
+++ src/main/java/server/MATE/domain/users/service/UsersService.java
@@ -0,0 +1,25 @@
+package server.MATE.domain.users.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import server.MATE.domain.users.dto.response.MeResponse;
+import server.MATE.domain.users.entity.Users;
+import server.MATE.domain.users.repository.UsersRepository;
+import server.MATE.global.common.exception.BaseErrorCode;
+import server.MATE.global.common.exception.BaseException;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class UsersService {
+
+    private final UsersRepository usersRepository;
+
+    public MeResponse getMe(Long userId) {
+        Users user = usersRepository.findById(userId)
+                .orElseThrow(() -> new BaseException(BaseErrorCode.AUTH_004));
+
+        return MeResponse.from(user);
+    }
+}
