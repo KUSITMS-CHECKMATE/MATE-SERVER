03256e9337cf4f4f0353fcc33871f4bac744c70b
happine2s <happine2s@sookmyung.ac.kr>
2026-05-07 17:59:37 +0900

    refactor: soft delete 조회 정책을 repository 메서드로 수정 (#28)

--- src/main/java/server/MATE/domain/question/entity/Question.java
+++ src/main/java/server/MATE/domain/question/entity/Question.java
@@ -5,7 +5,6 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.Where;
 import server.MATE.global.common.entity.BaseEntity;
 
 import java.time.LocalDateTime;
@@ -16,7 +15,6 @@ import java.time.LocalDateTime;
         name = "question",
         uniqueConstraints = @UniqueConstraint(columnNames = {"test_id", "sequence"})
 )
-@Where(clause = "deleted_at is null")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Question extends BaseEntity {
 
