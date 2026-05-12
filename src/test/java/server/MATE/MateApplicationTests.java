1b92cff9f598e62c6226418d70400fcf7ac01106
rossenzii <jangmj80@naver.com>
2026-05-12 17:34:54 +0900

    fix: Spring 테스트 실패 수정(#70)

--- src/test/java/server/MATE/MateApplicationTests.java
+++ src/test/java/server/MATE/MateApplicationTests.java
@@ -2,9 +2,16 @@ package server.MATE;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import server.MATE.global.image.ImageService;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MateApplicationTests {
+
+	@MockitoBean
+	private ImageService imageService;
 
 	@Test
 	void contextLoads() {
