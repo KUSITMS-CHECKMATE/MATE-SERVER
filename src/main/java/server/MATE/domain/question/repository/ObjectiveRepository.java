894ec32cd22530182ffb2685494bde96b6581590
hyunbin <s9476721@naver.com>
2026-05-12 14:59:50 +0900

    feat: 객관식 응답 등록 서비스 구현 (#59)

--- src/main/java/server/MATE/domain/question/repository/ObjectiveRepository.java
+++ src/main/java/server/MATE/domain/question/repository/ObjectiveRepository.java
@@ -2,12 +2,19 @@ package server.MATE.domain.question.repository;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import server.MATE.domain.question.entity.Objective;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ObjectiveRepository extends JpaRepository<Objective, Long> {
 
     @EntityGraph(attributePaths = "options")
     List<Objective> findAllByIdIn(Iterable<Long> ids);
+
+    @EntityGraph(attributePaths = "options")
+    @Query("SELECT o FROM Objective o WHERE o.id = :id")
+    Optional<Objective> findWithOptionsById(@Param("id") Long id);
 }
