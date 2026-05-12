4594cd13cd53e5a79b9e7cae764e6c52ceb247cd
hyunbin <s9476721@naver.com>
2026-05-12 18:57:02 +0900

    feat: FiveSecond 선택지 포함 조회 메서드 추가 (#60)

--- src/main/java/server/MATE/domain/question/repository/FiveSecondRepository.java
+++ src/main/java/server/MATE/domain/question/repository/FiveSecondRepository.java
@@ -2,12 +2,19 @@ package server.MATE.domain.question.repository;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import server.MATE.domain.question.entity.FiveSecond;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FiveSecondRepository extends JpaRepository<FiveSecond, Long> {
 
     @EntityGraph(attributePaths = "options")
     List<FiveSecond> findAllByIdIn(Iterable<Long> ids);
+
+    @EntityGraph(attributePaths = "options")
+    @Query("SELECT f FROM FiveSecond f WHERE f.id = :id")
+    Optional<FiveSecond> findWithOptionsById(@Param("id") Long id);
 }
