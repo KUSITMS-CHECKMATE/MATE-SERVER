3881ccb9425c97d1b276fafc4f88da2092e82fb1
happine2s <happine2s@sookmyung.ac.kr>
2026-05-14 07:05:50 +0900

    refactor: 토큰 저장소 인터페이스를 역할별로 분리 (#83)

--- src/main/java/server/MATE/domain/auth/service/RefreshTokenStore.java
+++
@@ -1,8 +0,0 @@
-package server.MATE.domain.auth.service;
-
-import server.MATE.domain.auth.jwt.TokenType;
-
-public interface RefreshTokenStore {
-
-    void save(Long userId, String refreshToken, TokenType tokenType, long ttlMillis);
-}
