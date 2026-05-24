f92bd92bfd825c04b1db900e06ea363011755442
happine2s <happine2s@sookmyung.ac.kr>
2026-05-25 04:49:51 +0900

    fix: approvalTime 필드로 수정 (#146)

--- src/main/java/server/MATE/domain/payment/repository/PaymentRefundRepository.java
+++ src/main/java/server/MATE/domain/payment/repository/PaymentRefundRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 
 public interface PaymentRefundRepository extends JpaRepository<PaymentRefund, Long> {
 
-    List<PaymentRefund> findAllByPaymentIdOrderByRefundedAtDesc(Long paymentId);
+    List<PaymentRefund> findAllByPaymentIdOrderByApprovalTimeDesc(Long paymentId);
 
     Optional<PaymentRefund> findByRefundNo(String refundNo);
 }
