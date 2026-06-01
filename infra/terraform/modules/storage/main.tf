2ed6e5cf786e3fb63266cc661e6cf8628bc49cd5
hyunbin <s9476721@naver.com>
2026-06-01 21:16:59 +0900

    fix: Azure Blob Storage CORS 설정 추가 (#205)

--- infra/terraform/modules/storage/main.tf
+++ infra/terraform/modules/storage/main.tf
@@ -6,6 +6,18 @@ resource "azurerm_storage_account" "this" {
   account_replication_type = var.account_replication_type
 }
 
+resource "azurerm_storage_account_blob_service_properties" "this" {
+  storage_account_id = azurerm_storage_account.this.id
+
+  cors_rule {
+    allowed_headers    = ["*"]
+    allowed_methods    = ["GET", "PUT", "OPTIONS"]
+    allowed_origins    = var.blob_cors_allowed_origins
+    exposed_headers    = ["*"]
+    max_age_in_seconds = 86400
+  }
+}
+
 resource "azurerm_storage_container" "app" {
   name                  = var.storage_container_name
   storage_account_name  = azurerm_storage_account.this.name
