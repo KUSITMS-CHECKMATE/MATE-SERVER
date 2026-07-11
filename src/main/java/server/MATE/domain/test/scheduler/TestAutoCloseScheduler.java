b745e76138c5b61d16885a662e5262e17cb5fc9a
rossenzii <jangmj80@naver.com>
2026-07-12 05:34:38 +0900

    fix: TestAutoCloseScheduler에 SchedulerLock 추가(#249)

--- src/main/java/server/MATE/domain/test/scheduler/TestAutoCloseScheduler.java
+++ src/main/java/server/MATE/domain/test/scheduler/TestAutoCloseScheduler.java
@@ -2,6 +2,7 @@ package server.MATE.domain.test.scheduler;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import server.MATE.domain.test.service.TestAutoCloseService;
@@ -14,6 +15,7 @@ public class TestAutoCloseScheduler {
     private final TestAutoCloseService testAutoCloseService;
 
     @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
+    @SchedulerLock(name = "testAutoCloseScheduler", lockAtMostFor = "PT10M")
     public void autoCloseExpiredTests() {
         log.info("테스트 자동 종료 스케줄러 실행");
         testAutoCloseService.closeExpiredTests();
