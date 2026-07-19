cb6cc17bb6f55ac86b700778c1a0df1eb525f7e2
happine2s <happine2s@sookmyung.ac.kr>
2026-07-19 21:50:30 +0900

    refactor: 관리자 인가를 경로 기반으로 통일 (#246)

--- src/main/java/server/MATE/domain/payment/controller/AdminPaymentController.java
+++ src/main/java/server/MATE/domain/payment/controller/AdminPaymentController.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +19,6 @@ public class AdminPaymentController {
 
     private final RefundService refundService;
 
-    @PreAuthorize("hasRole('ADMIN')")
     @Operation(
             summary = "환불 완료 처리",
             description = """
