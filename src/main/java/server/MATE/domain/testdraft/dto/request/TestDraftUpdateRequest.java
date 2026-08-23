91cddc9b41690dc74964c1df2895cdb06c234d26
happine2s <happine2s@sookmyung.ac.kr>
2026-08-24 00:31:17 +0900

    chore: 테스트 제목·소개·서비스 소개 글자수 제한 삭제 (#296)

--- src/main/java/server/MATE/domain/testdraft/dto/request/TestDraftUpdateRequest.java
+++ src/main/java/server/MATE/domain/testdraft/dto/request/TestDraftUpdateRequest.java
@@ -9,16 +9,13 @@ import server.MATE.domain.testdraft.validator.ClosedAtFormat;
 import java.util.List;
 
 public record TestDraftUpdateRequest(
-        @Size(max = 17, message = "테스트 이름은 최대 17자까지 입력 가능합니다.")
         String title,
 
-        @Size(max = 60, message = "테스트 한줄 소개는 최대 60자까지 입력 가능합니다.")
         String description,
 
         @Size(max = 17, message = "서비스 이름은 최대 17자까지 입력 가능합니다.")
         String serviceName,
 
-        @Size(max = 70, message = "서비스 소개는 최대 70자까지 입력 가능합니다.")
         String serviceDescription,
 
         @Size(max = 10, message = "이미지는 최대 10개까지 등록 가능합니다.")
